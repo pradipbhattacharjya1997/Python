6d64df2b6285238f19db0b14203f4966ab81bda3
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-02-26 11:33:03 +0000

    list condition optional

---
+++ List/Methods/listcompre.py
@@ -0,0 +1,15 @@
+"""
+[expression for item in iterable if condition]
+
+e-x * 2
+item-
+iterable - range(1,11)
+condition optional
+
+squares = []
+for i in range(1, 11):
+    squares.append(i ** 2)
+print(squares)
+"""
+squares = [i ** 2 for i in range(1,11) if i%2 == 0]
+print(squares)
