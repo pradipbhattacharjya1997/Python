655af597d766d158d62c5980b5f5ce48fc7e7de9
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-03-25 00:26:17 +0000

    loop pattern problem

---
+++ pattern problems/qs1.py
@@ -0,0 +1,5 @@
+for i in range(1,6): #rows
+    for j in range(1,i+1): #columns
+        print(j,end = "")
+    print()
+  
---
+++ pattern problems/qs6.py
@@ -0,0 +1,9 @@
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*",end = " ")
+    print ()
+
+for i in range(5, 0,-1):
+    for k in range (0,i-1):
+        print("*",end = " ")
+    print ()
