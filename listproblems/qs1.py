03931aca3d413243716ed70d6e0f2fb0de127396
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-03-25 09:45:05 +0000

    listproblems

---
+++ listproblems/qs1.py
@@ -0,0 +1,29 @@
+A= ["Ross", "Rachel", "Monica", "Joe"]
+
+# 1. Write a program to swap first and forth element.
+# A[0],A[3]=A[3],A[0]
+# print(A)
+
+# 2. Write a program to add a new value at second position.
+# A.insert(1,"Phoebe")
+# print(A)
+
+# 3. Write a program to delete a value from 3rd position.
+# A.pop(2)
+# print(A)
+B=[13,7,12, 10]
+
+# 1. Write a program to multiply all the numbers in the list.
+mul = 1
+for i in (B):
+    mul*=i
+print(mul)
+
+# 2. Write a program to get the largest number from the list.
+B.sort()
+print(B)
+print("The largest value in the given list is",B[-1])
+
+# 3. Write a prugram to get the smallest number from the list. 
+B.sort()
+print(B)
